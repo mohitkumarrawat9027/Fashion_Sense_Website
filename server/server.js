731de43a1bf817607cf328414d02f8f54d35eec5
mkr9027 <mkr9027@gmail.com>
2025-12-07 14:22:00 +0530

    cors error fixed

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5001;
 
 app.use(express.json());
 app.use(cors({
-  origin: 'https://fashionsensewebsite.netlify.app/',
+  origin: 'https://fashionsensewebsite.netlify.app',
   credentials: true
 }));
 
